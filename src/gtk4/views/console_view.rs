dcafd568e4f7557f831d5c301db384e32ffe90bc
DrBrad <brad@bradeagle.com>
2026-01-06 21:41:36 -0700

    Percentages

--- src/gtk4/views/console_view.rs
+++ src/gtk4/views/console_view.rs
@@ -1,4 +1,6 @@
+use glib::Propagation;
 use gtk4::{gdk, style_context_add_provider_for_display, Builder, ComboBoxText, CssProvider, Paned, ScrolledWindow};
+use gtk4::prelude::{RangeExt, ScaleExt};
 
 pub struct ConsoleView {
     pub root: gtk4::Box
@@ -16,6 +18,35 @@ impl ConsoleView {
         let root: gtk4::Box = builder
             .object("root")
             .expect("Couldn't find 'root' in console_view.ui");
+
+
+
+
+        let ghost_speed: gtk4::Scale = builder
+            .object("ghost_speed")
+            .expect("Couldn't find 'ghost_speed' in console_view.ui");
+
+        const SPEED_LABELS: [&str; 5] = [
+            "50%",
+            "75%",
+            "100%",
+            "125%",
+            "150%",
+        ];
+
+        ghost_speed.set_format_value_func(|_, value| {
+            let idx = value.round().clamp(0.0, 4.0) as usize;
+            SPEED_LABELS[idx].to_string()
+        });
+
+
+
+
+
+
+
+
+
 
         Self {
             root
